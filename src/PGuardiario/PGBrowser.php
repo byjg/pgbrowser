5fdaed136bc39ab8b6a207bbaa1796eadf725212
Joao Gilberto Magalhães <joao@byjg.com.br>
2016-08-31 18:30:06 -0300

    Changed the PGBrowser UserAgent version

--- src/PGuardiario/PGBrowser.php
+++ src/PGuardiario/PGBrowser.php
@@ -72,7 +72,7 @@ class PGBrowser{
   function __construct(){
 	$this->cookieFile = tempnam("/tmp", "COOKIE");
     $this->ch = curl_init();
-    curl_setopt($this->ch, CURLOPT_USERAGENT, "PGBrowser/0.0.1 (http://github.com/byjg/pgbrowser/)");
+    curl_setopt($this->ch, CURLOPT_USERAGENT, "PGBrowser/0.0.2 (http://github.com/byjg/pgbrowser/)");
     curl_setopt($this->ch, CURLOPT_FOLLOWLOCATION, true);
     curl_setopt($this->ch, CURLOPT_AUTOREFERER, true);
     curl_setopt($this->ch, CURLOPT_MAXREDIRS, 10);
